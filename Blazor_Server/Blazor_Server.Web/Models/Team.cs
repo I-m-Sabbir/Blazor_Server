7673f48db4790b6ff1c7a45f5ad64ee4f468bc7e
sabbir ahammed <xdxsabbir@gmail.com>
2023-01-12 17:25:39 +0600

    Initial Commit

---
+++ Blazor_Server/Blazor_Server.Web/Models/Team.cs
@@ -0,0 +1,10 @@
+﻿namespace Blazor_Server.Web.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Region { get; set; } = null!;
+        public List<TeamMember>? Members { get; set; }
+    }
+}
---
+++ Blazor_Server/Blazor_Server.Web/Models/TeamMember.cs
@@ -0,0 +1,10 @@
+﻿namespace Blazor_Server.Web.Models
+{
+    public class TeamMember
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int Age { get; set; }
+        public int TeamId { get; set; }
+    }
+}
